3abd30fd83427bd328b6b5d095f1fb0644edd532
Michal Sienkiewicz <s73271@beuth-hochschule.de>
2019-04-29 15:21:10 +0200

    Aufgabe C: checkout error

--- src/main/java/com/businessapp/Application.java
+++ src/main/java/com/businessapp/Application.java
@@ -8,9 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main( String[] args ) {
-		System.err.println( "\nHello, latest version of SpringApplication!\n" );
+		// System.err.println( "\nHello, latest version of SpringApplication!\n" );
+		System.out.println("\nHello, Michal Sienkiwicz!\n");
 		SpringApplication.run( Application.class, args );
-		System.err.println( "\nBye, SpringApplication!\n" );
+		System.out.println("\nBye, Michal Sienkiwicz!\n");
+		// System.err.println( "\nBye, SpringApplication!\n" );
+
+
 	}
 
 }
